11f6233e5cf9087e6356504eb548115f7430535b
118010362 <118010362@link.cuhk.edu.cn>
2024-07-02 15:53:55 +0800

    complete crud

--- core-java/Person.java
+++ core-java/Person.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Person {
+public class Person {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setName("John");
+        person.setAge(20);
+
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+    }
 }
